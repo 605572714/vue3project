abc9a1ef7a70cf6616748a3b40ec350e3a15b4cb
605572714 <605572714@qq.com>
2021-11-11 18:12:18 +0800

    父子组件
    父子组件

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,14 +2,23 @@
   <div>这是首页</div>
   <TodoList></TodoList>
   <FavIcon></FavIcon>
-  <h1 @click="toggle">click</h1>
+  <Rate v-model="score" theme="yellow">
+    <img src="../assets/avatar.png" width="16" alt />
+  </Rate>
+  <!-- <h1 @click="toggle">click</h1> -->
 </template>
 
 <script setup>
 import TodoList from '../components/TodoList.vue';
 import FavIcon from '../components/FavIcon.vue';
-import { useFullscreen } from "@vueuse/core"
+import Rate from '../components/Rate.vue';
+import { useFullscreen } from "@vueuse/core";
+import { ref } from 'vue';
 const { isFullscreen, enter, exit, toggle } = useFullscreen()
+let score = ref(2)
+// function update (num) {
+//   score.value = num
+// }
 </script>
 
 <style lang="scss" scoped>
