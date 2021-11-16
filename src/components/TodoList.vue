22e6f359605903d30353ba9853c2e9395866c7b4
Theseus <605572714@qq.com>
2021-11-16 18:09:57 +0800

    动画
    动画

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,10 +2,12 @@
   <div>
     <input type="text" v-model="title" @keydown.enter="addTodo" />
     <ul v-if="todos.length">
-      <li v-for="todo in todos" :key="todo">
-        <input type="checkbox" v-model="todo.done" />
-        <span :class="{ done: todo.done }">{{ todo.title }}</span>
-      </li>
+      <transition-group name="flip-list" tag="ul">
+        <li v-for="todo in todos" :key="todo">
+          <input type="checkbox" v-model="todo.done" />
+          <span :class="{ done: todo.done }">{{ todo.title }}</span>
+        </li>
+      </transition-group>
     </ul>
     <div v-else>暂无数据</div>
     <div>
@@ -13,18 +15,30 @@
       <input type="checkbox" v-model="allDone" />
       <span>{{ active }}/{{ all }}</span>
     </div>
+    <transition name="modal">
+      <div class="info-wrapper" v-if="showModal">
+        <div class="info">未输入任何消息</div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script setup>
 import { ref, computed, watchEffect } from 'vue'
 
-let { title, todos, addTodo, clear, active, all, allDone } = useTodos()
-
+let { title, showModal, todos, addTodo, clear, active, all, allDone } = useTodos()
 function useTodos () {
   let title = ref("")
+  let showModal = ref(false)
   let todos = useStorage('todos', [{ title: '学习vue', done: false }])
   function addTodo () {
+    if (!title.value) {
+      showModal.value = true;
+      setTimeout(() => {
+        showModal.value = false
+      }, 1500);
+      return;
+    }
     todos.value.push({
       title: title.value,
       done: false
@@ -48,7 +62,7 @@ function useTodos () {
       })
     }
   })
-  return { title, todos, addTodo, clear, active, all, allDone }
+  return { title, showModal, todos, addTodo, clear, active, all, allDone }
 }
 
 function useStorage (name, value = []) {
@@ -60,9 +74,39 @@ function useStorage (name, value = []) {
 }
 
 </script>
-
-<style scoped>
+<style>
 h1 {
   color: red;
+}
+.info-wrapper {
+  position: fixed;
+  top: 20px;
+  width: 200px;
+}
+.info {
+  padding: 20px;
+  color: white;
+  background: #d88986;
+}
+.modal-enter-from,
+.modal-leave-to {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+.modal-enter-active,
+.modal-leave-active {
+  transition: all 0.3s ease;
+}
+.flip-list-move {
+  transition: transform 0.8s ease;
+}
+.flip-list-enter-active,
+.flip-list-leave-active {
+  transition: all 1s ease;
+}
+.flip-list-enter-from,
+.flip-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
 }
 </style>
